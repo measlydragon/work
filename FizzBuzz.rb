478155b4515e8844416e27eebf668f5d0a46bf1c
measlydragon <measlydragon@users.noreply.github.com>
2016-05-17 22:27:54 -0400

    Add files via upload

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,7 +1,13 @@
-if n % 3 == 0 
-  print "Fizz"
-  elsif n % 5 == 0
-  print "Buzz"
-elsif (n % 3 == 0) && (n % 5 == 0)
-  print "FizzBuzz"
-else print num
+for n in (1..100)
+if  n % 3 == 0 && n % 5 == 0
+      print "FizzBuzz, "
+    elsif
+      n % 3 == 0
+   	  print "Fizz, "
+    elsif
+      n % 5 == 0 
+      print "Buzz, "
+    else
+      print "#{n}, "
+	end
+end
